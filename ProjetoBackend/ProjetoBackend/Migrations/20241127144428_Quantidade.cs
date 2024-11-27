cf6a91f3382d206e1d727eb3f7097c8deb075e02
Brun0HM <brunodev41@gmail.com>
2024-11-27 14:09:55 -0300

    Migrations

---
+++ ProjetoBackend/ProjetoBackend/Migrations/20241127144428_Quantidade.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Quantidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantidade",
+                table: "Compras",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Compras");
+        }
+    }
+}
